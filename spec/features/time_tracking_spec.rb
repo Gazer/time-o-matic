779f632815ffd3c206f12190cc92592011893ce5
Ricardo Markiewicz <gazer.arg@gmail.com>
2016-06-03 00:31:48 -0300

    New scenario and travil CI support

--- spec/features/time_tracking_spec.rb
+++ spec/features/time_tracking_spec.rb
@@ -8,4 +8,13 @@ feature "Time tracking", js: true do
 
     expect(page).to have_content("nombre de la tarea")
   end
+
+  scenario "stop current track" do
+    TrackedTime.create name: 'tracking'
+    visit '/'
+    click_on 'Finalizar'
+
+    expect(page).to_not have_content("nombre de la tarea")
+    expect(page).to have_content("Empezar")
+  end
 end
